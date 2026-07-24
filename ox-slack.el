4f15111f9007f40104f8c74a14dc5bff1833df49
Lucas Christian <lucas@lucasec.com>
2026-07-23 22:40:24 -0700

    Improve link handling.
    Re-writes the link handler. The following changes/improvements are
    made:
    
    - Links now support Slack's markdown-like syntax for specifying the
      link title [title](url).
    
    - Support for links to email addresses. If no description, just emits
      the email address verbatim, which Slack recognizes.
    
    - Special handling for links to local files, whether file: or via
      org-id. Obviously, people you message on Slack can't follow these,
      and they probably don't care about your filenames. So just emit the
      description in bold, or if no description, begrudgingly, your
      filename.
    
    - Slack has no anchors, so if you link to another headline, just emit
      the headline name in bold. If you put a description,
      emit *Description* [Headline name].
    
    - For links that have no exportable form, e.g. gnus, just emit the
      description.

--- ox-slack.el
+++ ox-slack.el
@@ -96,83 +96,67 @@ a communication channel."
 
 ;; link
 (defun org-slack-link (link contents info)
-  "Transcode LINK object into Markdown format.
-  CONTENTS is the link's description.  INFO is a plist used as
-  a communication channel."
-  (let ((link-org-files-as-md
-         (lambda (raw-path)
-           ;; Treat links to `file.org' as links to `file.md'.
-           (if (string= ".org" (downcase (file-name-extension raw-path ".")))
-               (concat (file-name-sans-extension raw-path) ".md")
-             raw-path)))
-        (type (org-element-property :type link)))
+  "Transcode LINK object into Slack format.
+CONTENTS is the link's description.  INFO is a plist used as a
+communication channel."
+  (let ((type (org-element-property :type link))
+        (path (org-element-property :path link))
+        (desc (org-string-nw-p contents)))
     (cond
-     ;; Link type is handled by a special function.
-     ((org-export-custom-protocol-maybe link contents 'md))
-     ((member type '("custom-id" "id" "fuzzy"))
-      (let ((destination (if (string= type "fuzzy")
-                             (org-export-resolve-fuzzy-link link info)
-                           (org-export-resolve-id-link link info))))
-        (pcase (org-element-type destination)
-          (`plain-text			; External file.
-           (let ((path (funcall link-org-files-as-md destination)))
-             (if (not contents) (format "%s>" path)
-               (format "[%s](%s)" contents path))))
+     ;; Routable web links: Use Slack URL syntax if has a description
+     ((member type '("http" "https" "ftp"))
+      (let ((url (concat type ":" path)))
+        (if desc (format "[%s](%s)" desc url) url)))
+     ;; Email addresses: use a mailto link if has a description
+     ((string= type "mailto")
+      (if desc (format "[%s](mailto:%s)" desc path) path))
+     ;; References to local files, other locations
+     ((member type '("file" "custom-id" "id" "fuzzy"))
+      ;; Resolve the link to its destination, either the path to
+      ;; another local file, or a headline/location
+      (let ((dest (pcase type
+                    ("file" path)
+                    ("fuzzy" (org-export-resolve-fuzzy-link link info))
+                    (_ (org-export-resolve-id-link link info)))))
+        (pcase (org-element-type dest)
+          ;; Local file path: just emit the link description, or if no
+          ;; description the base name of the file.  These can't
+          ;; actually link anywhere useful and leaking our internal
+          ;; directory structure just adds noise.
+          (`plain-text
+           (format "*%s*"
+                   (or desc (file-name-nondirectory (directory-file-name dest)))))
+          ;; Headline within the export region: Slack doesn't have
+          ;; anchors, so just emit the description or headline title
+          ;; in bold.  If the description doesn't match the title,
+          ;; include the headline title too.
           (`headline
-           (format
-            ;; "[%s](#%s)"
-            "[%s]"
-            ;; Description.
-            (cond ((org-string-nw-p contents))
-                  ((org-export-numbered-headline-p destination info)
-                   (mapconcat #'number-to-string
-                              (org-export-get-headline-number destination info)
-                              "."))
-                  (t (org-export-data (org-element-property :title destination)
-                                      info)))
-            ;; Reference.
-            ;; (or (org-element-property :CUSTOM_ID destination)
-            ;;     (org-export-get-reference destination info))
-            ))
+           (let ((title (org-export-data (org-element-property :title dest) info)))
+             (if (and desc (not (string= desc title)))
+                 (format "*%s* [%s]" desc title)
+               (format "*%s*" (or desc title)))))
+          ;; Exotic targets (tables, figures): emit the description,
+          ;; or else the target's ordinal number in [brackets].
           (_
-           (let ((description
-                  (or (org-string-nw-p contents)
-                      (let ((number (org-export-get-ordinal destination info)))
-                        (cond
-                         ((not number) nil)
-                         ((atom number) (number-to-string number))
-                         (t (mapconcat #'number-to-string number ".")))))))
-             (when description
-               (format "[%s]"
-                       description
-                       ;; (org-export-get-reference destination info)
-                       )))))))
-     ((org-export-inline-image-p link org-html-inline-image-rules)
-      (let ((path (let ((raw-path (org-element-property :path link)))
-                    (cond ((not (equal "file" type)) (concat type ":" raw-path))
-                          ((not (file-name-absolute-p raw-path)) raw-path)
-                          (t (expand-file-name raw-path)))))
-            (caption (org-export-data
-                      (org-export-get-caption
-                       (org-export-get-parent-element link)) info)))
-        (format "![img](%s)"
-                (if (not (org-string-nw-p caption)) path
-                  (format "%s \"%s\"" path caption)))))
+           (or (and desc (format "*%s*" desc))
+               (let ((number (org-export-get-ordinal dest info)))
+                 (and number
+                      (format "[%s]"
+                              (if (atom number) (number-to-string number)
+                                (mapconcat #'number-to-string number "."))))))))))
+     ;; Coderef
      ((string= type "coderef")
-      (let ((ref (org-element-property :path link)))
-        (format (org-export-get-coderef-format ref contents)
-                (org-export-resolve-coderef ref info))))
-     ((equal type "radio") contents)
-     (t (let* ((raw-path (org-element-property :path link))
-               (path
-                (cond
-                 ((member type '("http" "https" "ftp" "mailto"))
-                  (concat type ":" raw-path))
-                 ((string= type "file")
-                  (org-export-file-uri (funcall link-org-files-as-md raw-path)))
-                 (t raw-path))))
-          (if (not contents) (format "%s" path)
-            (format "*%s* (%s)" contents path)))))))
+      (format (org-export-get-coderef-format path contents)
+              (org-export-resolve-coderef path info)))
+     ;; Radio target
+     ((string= type "radio") contents)
+     ;; Custom types with an export handler: Call the type's own
+     ;; exporter, suggesting it export Markdown (which will hopefully
+     ;; be compatible).
+     ((org-export-custom-protocol-maybe link contents 'md))
+     ;; Any other type (gnus, etc.): keep just the description in bold
+     (desc (format "*%s*" desc))
+     (t path))))
 
 (defun org-slack-verbatim (_verbatim contents _info)
   "Transcode VERBATIM from Org to Slack.
